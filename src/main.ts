16f10d8a21e08eb5dd8d32c9034cde62333cc3e1
Gelaris <Gelaris0@yandex.ru>
2024-12-13 15:44:13 +0300

    6verbac

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: '*',  // Временно разрешаем все источники
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
   });
   
   await app.listen(process.env.PORT || 3000);
